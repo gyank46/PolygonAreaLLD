6536bc0a95ef217ddf679e6317318223cbe86f87
gyan prakash <lebowski@192.168.1.97>
2022-08-13 22:26:24 +0530

    client change

--- src/main/java/com/example/Factory/Client.java
+++ src/main/java/com/example/Factory/Client.java
@@ -5,8 +5,8 @@ public class Client {
     public static void main(String[] args) {
         String databaseType = DataBases.MongoDB.toString();
         Database db = DataBaseFactory.getDataBaseByType(databaseType);
-//        Connection con = db.getDatabaseFactoryMethods().getConnection();
-//        Transaction transaction =  db.getDatabaseFactoryMethods().getTransaction();
+        Connection con = db.getDatabaseFactoryMethods().getConnection();
+        Transaction transaction =  db.getDatabaseFactoryMethods().getTransaction();
         DatabaseFactoryMethods sql1= db.getDatabaseFactoryMethods();
         System.out.println(sql1);
         sql1 = db.getDatabaseFactoryMethods();
